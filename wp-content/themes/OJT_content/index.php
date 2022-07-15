29c88efd1ff4e14c91d5c11591799ec5068c1be3
Haruna Kumon <>
2022-07-15 19:48:16 +0900

    no message

--- wp-content/themes/OJT_content/index.php
+++ wp-content/themes/OJT_content/index.php
@@ -14,70 +14,24 @@
     <!-- </div> -->
     <p class="main-font">進化し続ける「街」アメリカンビレッジマガジン</p>
     </div>
-
-    <h1>Latest Articles</h1>
+    <h1><?php the_title(); ?></h1>
+    <?php 
+    if(have_post()):
+    while(have_post()):
+    the_post();
+  ?>
     <section class="contents">
         <div class="contents-bx1">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_1.png" alt="">
+        <?php the_post_thumbnail(); ?>
+            <!-- <img src="<?php //echo get_template_directory_uri(); ?>/folder/post_img_1.png" alt=""> -->
             <div class="date">
                 <div class="blog">
-                    <time datetime="2018-05-20">2018 / 5 / 20</time>
-                    <p class="p">おしゃれなカフェがありますよ</p>
+                    <time datetime=""><?php the_date(); ?></time>
+                    <p class="p"><?php the_excerpt(); ?></p>
                 </div>
-                <a href="">READ MORE</a>
+                <a href="<?php the_permalink(); ?>">READ MORE</a>
             </div>
         </div>
-        <div class="contents-bx1">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_2.png" alt="">
-            <div class="date">
-                <div class="blog">
-                    <time datetime="2018-05-19">2018 / 5 / 19</time>
-                    <p class="p">あのネオンはいつ交換するのか！？観覧車の謎に迫る！</p>
-                </div>
-                <a href="">READ MORE</a>
-            </div>
-        </div>
-        <div class="contents-bx1">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_3.png" alt="">
-            <div class="date">
-                <div class="blog">
-                    <time datetime="2018-05-18">2018 / 5 / 18</time>
-                    <p class="p">ラソナの社内はこんなのよ</p>
-                </div>
-                <a href="">READ MORE</a>
-            </div>
-        </div>
-    </section>
-    <section class="contents-2">
-        <div class="contents-bx2">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_4.png" alt="">
-            <div class="date">
-                <div class="blog">
-                    <time datetime="2018-05-27">2018 / 5 / 27</time>
-                    <p class="p">お隣のアラハはハワイ？</p>
-                </div>
-                <a href="">READ MORE</a>
-            </div>
-        </div>
-        <div class="contents-bx2">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_5.png" alt="">
-            <div class="date">
-                <div class="blog">
-                    <time datetime="2018-05-16">2018 / 5 / 16</time>
-                    <p class="p">なぜテント？ラソナの人に聞いてみた</p>
-                </div>
-                <a href="">READ MORE</a>
-            </div>
-        </div>
-        <div class="contents-bx2">
-            <img src="<?php echo get_template_directory_uri(); ?>/folder/post_img_6.png" alt="">
-            <div class="date">
-                <div class="blog">
-                    <time datetime="2018-05-15">2018 / 5 / 15</time>
-                    <p class="p">ベイエリアおしゃれすぎる問題</p>
-                </div>
-                <a href="">READ MORE</a>
-            </div>
-        </div>
-    </section>
+    <?php endwhile; ?>
+    <?php endif; ?>
     <?php get_footer(); ?>
